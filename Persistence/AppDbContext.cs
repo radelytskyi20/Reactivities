c3f541ce70265150a532dbad60ed82e5f7097992
Yaroslav Radelytskyi <yaroslav.radelytskyi@gmail.com>
2025-02-20 21:23:30 +0200

    Add project files.

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Activity = Domain.Models.Activity;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public required DbSet<Activity> Activities { get; set; }
+}
